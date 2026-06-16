f5e51c1353b151a94e1d973aedf898a8745ab1de
Jose rafael <joserafaelmarenco@gmail.com>
2026-06-16 11:24:11 -0500

    TASK 1

---
+++ manejo.js
@@ -0,0 +1,7 @@
+const productos = {
+  p1: { id: "p1", nombre: "Teclado mecánico", precio: 150000 },
+  p2: { id: "p2", nombre: "Mouse inalámbrico", precio: 80000 },
+  p3: { id: "p3", nombre: "Monitor 24 pulgadas", precio: 650000 },
+  p4: { id: "p4", nombre: "Audífonos Bluetooth", precio: 120000 },
+};
+ 
